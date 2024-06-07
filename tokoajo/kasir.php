7cbbb68c16008497e9391c64f5ec6db1b576ca87
Zain <s4tr14b1m4131225>
2024-06-07 22:16:00 +0700

    nyoba tutorial yang lain

---
+++ tokoajo/kasir.php
@@ -0,0 +1,25 @@
+<section class="content-header">
+    <h1>Sales
+        <small>Penjualan</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+    <li>Transaction</li>
+    <li class="active">Sales</li>
+</ol>
+</section>
+
+<section class="content">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="box box-widget">
+                <div class="box-body">
+                    <table>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
--- tokoajo/kasir2.php
+++ tokoajo/kasir2.php
@@ -58,85 +58,39 @@
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
-                        <div class="small">Login Sebagai:</div>
-                        Admin
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
                     </div>
                 </nav>
             </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Transaksi</h1>
-                        <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-area me-1"></i>
-                                        Area Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-bar me-1"></i>
-                                        Bar Chart Example
-                                    </div>
-                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                DataTable Example
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>s
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
+                        <div id="layoutSidenav_content">
+                         <main>
+                            <div class="container-fluid px-4">
+                            <h1>Transaksi
+                            </h1>
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="box box-widget">
+                                        <div class="box-body">    
+                                            <table width="100%">
+                                                <tr>
+                                                    <td style="vertical-align:top">
+                                                        <label for ="date">Tanggal</label>
+                                                    </td>
+                                                    <td>
+                                                        <div class="form-group">
+                                                            <input type="date" id="date" value="<?=date('Y-m-d')?>" class="form-control">
+                                                        </div>
+                                                    </td>   
+                                                        </tr>
+                                             </table>
+                                                        
+                                            </div>
+                                        <div class="box box-widget">
                                         
-                                    </tbody>
-                                </table>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
