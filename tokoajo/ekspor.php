148f2708d4d2477eaa518dc71545835504d29b47
coldweaver0 <zakyzaujan@gmail.com>
2024-06-08 22:11:52 +0700

    tambahkan fungsi export data produk.

---
+++ tokoajo/ekspor.php
@@ -0,0 +1,75 @@
+<?php
+require 'function.php';
+?>
+<html>
+<head>
+  <title>Data Produk Toko Ajo Lpn</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <br>
+    <h2>Data Produk Toko Ajo Lpn</h2>
+    <br>
+    <div class="data-tables datatable-dark">
+        <table id="datatablesSimple" class="display">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nama Produk</th>
+                    <th>Harga</th>
+                    <th>Stok</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $ambilsemuadataproduk = mysqli_query($koneksi, "SELECT * FROM produk");
+                $i = 1;
+                while ($data = mysqli_fetch_array($ambilsemuadataproduk)) {
+                    $namaproduk = $data['namaproduk'];
+                    $harga = $data['harga'];
+                    $stok = $data['stok'];
+                    $idproduk = $data['idproduk'];
+                ?>
+                <tr>
+                    <td><?= $i++; ?></td>
+                    <td><?php echo $namaproduk; ?></td>
+                    <td><?php echo $harga; ?></td>
+                    <td><?php echo $stok; ?></td>
+                </tr>
+                <?php
+                };
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<script>
+$(document).ready(function() {
+    $('#datatablesSimple').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+});
+</script>
+
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+</body>
+</html>
--- tokoajo/inventaris.php
+++ tokoajo/inventaris.php
@@ -1,5 +1,5 @@
 <?php
-require 'function.php'
+require 'function.php';
 ?>
 
 <!DOCTYPE html>
@@ -23,10 +23,6 @@ require 'function.php'
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-                </div>
             </form>
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
@@ -73,11 +69,31 @@ require 'function.php'
                         <h1 class="mt-4">Inventaris</h1>
                         <br>
                         <!-- Button to Open the Modal -->
-                            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#myModal">
+                            <button type="button" class="btn btn-success me-2" data-bs-toggle="modal" data-bs-target="#myModal">
                             <i class="fa-solid fa-square-plus"></i>
                              Tambahkan Produk
                             </button>
+                            <a href="ekspor.php" class="btn btn-secondary" target="_blank">
+                            <i class="fa-solid fa-print"></i>
+                             Ekspor Data</a>
                             <br><br>
+
+                            <?php
+                                $ambildatastok = mysqli_query($koneksi,"SELECT * FROM produk WHERE stok < 1");
+
+                                while($fetch=mysqli_fetch_array($ambildatastok)){
+                                    $produk = $fetch['namaproduk'];
+                                
+                            ?>
+                            <div class="alert alert-danger alert-dismissible">
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                <strong>Perhatian!</strong> Stok dari <b>"<?=$produk;?>"</b> telah habis.
+                            </div>
+
+                            <?php
+                                }
+                            ?>
+                            
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
@@ -134,12 +150,14 @@ require 'function.php'
                                                     <!-- Modal body -->
                                                     <form method="post">
                                                     <div class="modal-body">
+                                                    Nama Produk
                                                     <input type="text" name="namaproduk" value="<?=$namaproduk;?>" class="form-control" required>
-                                                    <br>
+                                                    
+                                                    Harga
                                                     <input type="number" name="harga" value="<?=$harga;?>" class="form-control" required>
-                                                    <br>
+                                                    
+                                                    Stok
                                                     <input type="number" name="stok" value="<?=$stok;?>" class="form-control" required>
-                                                    <br>
                                                     <input type="hidden" name="idproduk" value="<?=$idproduk;?>">
                                                         <div class="modal-footer">
                                                             <button type="submit" class="btn btn-warning" name="updateproduk">Update</button>
