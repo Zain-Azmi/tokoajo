099225e2bc4d5150f51696ddf1567c827b0b70e9
coldweaver0 <zakyzaujan@gmail.com>
2024-06-09 12:16:13 +0700

    tambah fungsi ekspor

--- tokoajo/ekspor.php
+++ tokoajo/ekspor.php
@@ -10,7 +10,7 @@ require 'function.php';
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
